17707c80466d9c80d0b0a08951511b9533c48b49
brianbaez <brianbaez.dev@gmail.com>
2022-02-07 14:37:15 -0600

    Implemented dark mode

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
+// Variables
 const navToggle = document.querySelector(".nav__toggle");
 const navLinks = document.querySelectorAll(".nav__link");
+const navDarkMode = document.querySelector(".nav__dark-mode");
+const root = document.querySelector(":root");
+
+var darkModeEnabled = false;
 
 navToggle.addEventListener("click", () => {
   console.log("Clicked");
@@ -10,4 +15,21 @@ navLinks.forEach(link => {
   link.addEventListener("click", () => {
     document.body.classList.remove("nav__open");
   });
+});
+
+navDarkMode.addEventListener("click", () => {
+  if(!darkModeEnabled) {
+    root.style.setProperty("--clr-primary", "#FFF");
+    root.style.setProperty("--clr-secondary", "#102734");
+
+    navDarkMode.classList.replace("fa-moon", "fa-sun");
+    darkModeEnabled = true;
+  }
+  else {
+    root.style.setProperty("--clr-primary", "#102734");
+    root.style.setProperty("--clr-secondary", "FFF");
+
+    navDarkMode.classList.replace("fa-sun", "fa-moon");
+    darkModeEnabled = false;
+  }
 });
