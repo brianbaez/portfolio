3ca6455a16c720f825e859c3691f4c2c1e52b676
brianbaez <brianbaez.dev@gmail.com>
2022-07-18 16:18:30 -0500

    Updated App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import {gsap} from "gsap";
+
+import '../assets/styles/App.scss';
 
 // Components
-import Navbar from "./Navbar";
-import Hero from "./Hero";
+import Navbar from "./Navbar/Navbar";
+import Hero from "./Hero/Hero";
 import AboutMe from "./AboutMe";
-import Skills from "./Skills";
-import Projects from "./Projects";
+import Skills from "./Skills/Skills";
+import Projects from "./Projects/Projects";
 import ContactMe from "./ContactMe";
 import Footer from "./Footer";
 
 function App() {
+  const heroIntroRef = useRef();
+  const socialMediaRef = useRef();
+  const navLogoRef = useRef();
+  const navListRef = useRef();
+  const navToggleRef = useRef();
+  const arrowRef = useRef();
+
+  useEffect(() => {
+    gsap.timeline()
+      .from(heroIntroRef.current, {opacity: 0, duration: 0.5, y: 75})
+      .from(socialMediaRef.current, {opacity: 0, duration: 0.5})
+      .from(navLogoRef.current, {opacity: 0, duration: 0.25, y: -50})
+      .from(navListRef.current, {opacity: 0, duration: 0.25, y: -50}, "-=0.25")
+      .from(navToggleRef.current, {opacity: 0, duration: 0.25, y: -50}, "-=0.25")
+      .from(arrowRef.current, {opacity: 0, duration: 0.25}, "=-0.25");
+  });
+
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
+      <header>
+        <Navbar
+          navLogoRef={navLogoRef}
+          navListRef={navListRef}
+          navToggleRef={navToggleRef}
+        />
+        <Hero
+          heroIntroRef={heroIntroRef}
+          socialMediaRef={socialMediaRef}
+          arrowRef={arrowRef}
+        />
+      </header>
       <AboutMe />
       <Skills />
       <Projects />
