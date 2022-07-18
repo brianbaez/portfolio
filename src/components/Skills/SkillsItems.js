9e9f50e8178f1f86e98e36b6b1d627ba9be9576a
brianbaez <brianbaez.dev@gmail.com>
2022-07-18 16:17:22 -0500

    Updated Skills component

---
+++ src/components/Skills/SkillsItems.js
@@ -0,0 +1,50 @@
+export const skillsItems = [
+  {
+    name: "HTML",
+    icon: "devicon-html5-plain colored"
+  },
+  {
+    name: "CSS",
+    icon: "devicon-css3-plain colored"
+  },
+  {
+    name: "JavaScript",
+    icon: "devicon-javascript-plain colored"
+  },
+  {
+    name: "Sass",
+    icon: "devicon-sass-original colored"
+  },
+  {
+    name: "Bootstrap",
+    icon: "devicon-bootstrap-plain colored"
+  },
+  {
+    name: "jQuery",
+    icon: "devicon-jquery-plain colored"
+  },
+  {
+    name: "MongoDB",
+    icon: "devicon-mongodb-plain colored"
+  },
+  {
+    name: "ExpressJS",
+    icon: "devicon-express-original colored"
+  },
+  {
+    name: "ReactJS",
+    icon: "devicon-react-original colored"
+  },
+  {
+    name: "NodeJS",
+    icon: "devicon-nodejs-plain colored"
+  },
+  {
+    name: "Git",
+    icon: "devicon-git-plain colored"
+  },
+  {
+    name: "Atom",
+    icon: "devicon-atom-original colored"
+  }
+];
