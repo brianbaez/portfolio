4f4085b6f54a34bdc93901a6f37da05a7d66f8a1
Brian Baez <brianbaez.dev@gmail.com>
2023-05-04 00:20:46 -0500

    Added Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Components
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <a className="social-media__footer fab fa-linkedin" href="https://www.linkedin.com/in/brianbaez/"></a>
+      <a className="social-media__footer fab fa-github-square" href="https://github.com/brianbaez"></a>
+      <a className="social-media__footer fas fa-envelope-square" href="mailto:brianbaez.bab@gmail.com"></a>
+      <p className="copyright">© 2022 Brian Baez</p>
+    </footer>
+  );
+}
+
+export default Footer;
