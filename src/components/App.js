aadae0c2a29ac30f1bbc49271afa6f5aa0896836
Brian Baez <brianbaez.dev@gmail.com>
2023-05-04 17:55:10 -0500

    Updates

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
