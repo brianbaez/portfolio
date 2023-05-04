03c96dc1d98fff6fcca4ad7b9a86470dcc74c06b
Brian Baez <brianbaez.dev@gmail.com>
2023-05-04 00:53:53 -0500

    Updated Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef} from "react";
-import {gsap} from "gsap";
+//import gsap from "gsap";
 
 // Components
 import Navbar from "./Navbar/Navbar";
@@ -18,15 +18,15 @@ function Home() {
     const navToggleRef = useRef();
     const arrowRef = useRef();
   
-    useEffect(() => {
-      gsap.timeline()
-        .from(heroIntroRef.current, {opacity: 0, duration: 0.5, y: 75})
-        .from(socialMediaRef.current, {opacity: 0, duration: 0.5})
-        .from(navLogoRef.current, {opacity: 0, duration: 0.25, y: -50})
-        .from(navListRef.current, {opacity: 0, duration: 0.25, y: -50}, "-=0.25")
-        .from(navToggleRef.current, {opacity: 0, duration: 0.25, y: -50}, "-=0.25")
-        .from(arrowRef.current, {opacity: 0, duration: 0.25}, "=-0.25");
-    });
+    // useEffect(() => {
+    //     gsap.timeline()
+    //         .from(heroIntroRef.current, {opacity: 0, duration: 0.5, y: 75})
+    //         // .from(socialMediaRef.current, {opacity: 0, duration: 0.5})
+    //         // .from(navLogoRef.current, {opacity: 0, duration: 0.25, y: -50})
+    //         // .from(navListRef.current, {opacity: 0, duration: 0.25, y: -50}, "-=0.25")
+    //         // .from(navToggleRef.current, {opacity: 0, duration: 0.25, y: -50}, "-=0.25")
+    //         // .from(arrowRef.current, {opacity: 0, duration: 0.25}, "=-0.25");
+    // });
 
     return (
         <div className="App">
