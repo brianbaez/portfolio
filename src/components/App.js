22f2bc4a7939f3ed3fdde91fd7e8f8b7164b067e
Brian Baez <brianbaez.dev@gmail.com>
2023-05-04 17:34:18 -0500

    Updated Route path

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/portfolio" element={<Home />} />
       </Routes>
     </Router>
   );
