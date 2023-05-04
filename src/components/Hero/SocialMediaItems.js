b76115f2eecc7eeead71dff2ec82b0aed83e007e
Brian Baez <brianbaez.dev@gmail.com>
2023-05-04 00:21:19 -0500

    Added SocialMediaItems.js

---
+++ src/components/Hero/SocialMediaItems.js
@@ -0,0 +1,16 @@
+export const socialMediaItems = [
+  {
+    href: "https://linkedin.com/in/brianbaez",
+    icon: "fab fa-linkedin",
+    target: "_blank"
+  },
+  {
+    href: "https://github.com/brianbaez",
+    icon: "fab fa-github-square",
+    target: "_blank"
+  },
+  {
+    href: "mailto:brianbaez.bab@gmail.com",
+    icon: "fas fa-envelope-square"
+  }
+];
