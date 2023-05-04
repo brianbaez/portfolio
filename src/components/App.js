d81528e4f852ff6495cfe1c42c49856a7190789e
Brian Baez <brianbaez.dev@gmail.com>
2023-05-04 17:40:52 -0500

    Added basename

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import Home from "./Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/portfolio" element={<Home />} />
       </Routes>
